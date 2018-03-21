74db5f26b1a1926f09beebdbd69e93562114e538
Andrey <33412680+ramirezjag00@users.noreply.github.com>
2018-03-21 15:20:46 +0800

    PROD_WORKING
    surveyTemplate edited.
    surveyRoutes edited.
    edited Prod sendgrid key.
    prod is now working

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -8,7 +8,7 @@ const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-const Mailer = require('../services/Mailer');
+const Mailer = require('../services/mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 module.exports = app => {
